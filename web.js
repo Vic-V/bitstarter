81ff11dbf8246c7b78b9ba9e499c3272b5a36c4f
Vic V <victvost@gmail.com>
2013-07-24 15:43:17 +0000

    Real username

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
 var fs = require('fs');
-
 var buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
